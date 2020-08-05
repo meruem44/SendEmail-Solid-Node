ccf5bd75605f65d85ee88af5b5d53cf0d5f70878
leandro moraes <leandromoraes12345@outlook.com>
2020-08-04 22:00:03 -0300

    create User model

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/export */
+import { uuid } from 'uuidv4'
+
+export class User {
+    public readonly id: string;
+
+    public name: string;
+    public email: string;
+    public password: string;
+
+    constructor (props: Omit<User, 'id'>, id?: string) {
+      Object.assign(this, props)
+
+      if (!id) {
+        this.id = uuid()
+      }
+    }
+}
+
+export default User
